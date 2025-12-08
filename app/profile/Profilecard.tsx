475be0c80ac5c215a7af86bc68874aaa619c9a22
FelienZ <rajabfadh@gmail.com>
2025-12-08 16:04:21 +0700

    optimize some page elements for profile pages

--- app/profile/Profilecard.tsx
+++ app/profile/Profilecard.tsx
@@ -10,14 +10,14 @@ export default function ProfileCard(){
     return(
         <div className="grid relative bg-card text-card-foreground transition-colors ease-in-out duration-200 drop-shadow-sm rounded-md">
                 <div className="upper flex relative h-[35vh] p-3 drop-shadow-sm">
-                    <Image sizes="100%" src="/images/my.jpg" loading="eager" fill className="object-cover rounded-t-sm brightness-75" alt=""/>
+                    <Image sizes="100%" src="/images/my.jpg" loading="eager" fill className="object-cover rounded-t-sm brightness-75" alt="waifu-pic-2"/>
                 </div>
                 <div className="lower md:px-10 rounded-b-sm">
                     <div className="flex max-sm:flex-col items-center gap-4 h-full">
-                        <Image onClick={()=>toast.warning("Ini My Gweh Woi")} sizes="100%" src="/images/takina3.jpg" width={200} height={200} className="object-cover hover:cursor-pointer relative max-sm:absolute sm:left-6 bottom-30 max-sm:bottom-45 p-3 bg-card text-card-foreground transition-colors ease-in-out duration-200 rounded-full" alt=""/>
+                        <Image onClick={()=>toast.warning("Ini My Gweh Woi")} sizes="100%" src="/images/takina3.jpg" width={200} height={200} className="object-cover hover:cursor-pointer relative max-sm:absolute sm:left-6 bottom-30 max-sm:bottom-45 p-3 bg-card text-card-foreground transition-colors ease-in-out duration-200 rounded-full" alt="waifu-pic-3"/>
                         <div className="flex flex-col h-full justify-evenly max-sm:pt-20 p-6 gap-4">
                             <div className="flex flex-col gap-2">
-                                <p className="font-bold md:text-xl max-md:text-center tracking-[3px]">M. Rajab Fadhly Hartawibawa</p>
+                                <h1 className="font-bold md:text-xl max-md:text-center tracking-[3px]">M. Rajab Fadhly Hartawibawa</h1>
                                 <div className="flex max-md:grid grid-cols-2 place-items-center sm:items-center gap-1">
                                     <Badge className="bg-neutral-500 text-white"><CodeXml/>Web Developer</Badge>
                                     <Badge className="bg-emerald-500 text-white"><GraduationCap/>Undergraduate Student</Badge>
--- app/profile/Stackcard.tsx
+++ app/profile/Stackcard.tsx
@@ -7,12 +7,12 @@ export default function StackCard(){
     const [loaded, setLoaded] = useState(false);
     return(
         <section className="flex flex-col gap-3 bg-card text-card-foreground transition-colors ease-in-out duration-200 rounded-md drop-shadow-sm p-4">
-            <p className="font-bold flex items-center gap-3"><Layers/> Dev&apos;s Tools (Technologiaa)</p>
+            <h2 className="font-bold flex items-center gap-3"><Layers/> Dev&apos;s Tools (Technologiaa)</h2>
                 <div className="inset-shadow-sm bg-card text-card-foreground transition-colors ease-in-out duration-200 border rounded-md p-1 relative overflow-hidden">
                     <motion.div className="flex w-full justify-evenly items-center" animate={{x:['100%', '-100%']}} transition={{duration: 20, ease: 'linear', repeat: Infinity, repeatType: 'reverse'}}>
                         {ToolsData.map((i, idx) => idx < 7 ? (
                             <div key={idx} className="relative stack-marquee size-10">
-                                <Image sizes="100%" loading="lazy" onLoad={()=>setLoaded(true)} src={i.url} alt="" className={`p-1 transition-opacity duration-200 ${loaded? 'opacity-100' : 'opacity-0'}`} fill/>
+                                <Image sizes="100%" loading="lazy" onLoad={()=>setLoaded(true)} src={i.url} alt={`Dev-tools-${idx}`} className={`p-1 transition-opacity duration-200 ${loaded? 'opacity-100' : 'opacity-0'}`} fill/>
                             </div>
                         ): '')}
                     </motion.div>
@@ -21,7 +21,7 @@ export default function StackCard(){
                     <motion.div className="flex w-full justify-evenly items-center" animate={{x:['-100%', '100%']}} transition={{duration: 20, ease: 'linear', repeat: Infinity, repeatType: 'reverse'}}>
                         {ToolsData.map((i, idx) => idx >= 7 ? (
                             <div key={idx} className="relative stack-marquee size-10">
-                                <Image sizes="100%" loading="lazy" onLoad={()=>setLoaded(true)} src={i.url} alt="" className={`p-1 transition-opacity duration-200 ${loaded? 'opacity-100' : 'opacity-0'}`} fill/>
+                                <Image sizes="100%" loading="lazy" onLoad={()=>setLoaded(true)} src={i.url} alt={`Dev-tools2-${idx}`} className={`p-1 transition-opacity duration-200 ${loaded? 'opacity-100' : 'opacity-0'}`} fill/>
                             </div>
                         ): '')}
                     </motion.div>
