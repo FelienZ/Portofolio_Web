585364d13212f24de6f1f158d8848c1dcb16fe94
FelienZ <rajabfadh@gmail.com>
2026-01-22 22:23:16 +0700

    refactor(types): update some item types

--- utils/types/data/featuredItem.ts
+++ utils/types/data/featuredItem.ts
@@ -1,7 +1,6 @@
-import { ReactElement } from "react";
 import { Iteminterface } from "./interfaceItem";
 
 export interface FeaturedItem extends Iteminterface{
     imgUrl: string,
-    icon: ReactElement
+    createdAt: string
 }
--- utils/types/data/statsItem.ts
+++ utils/types/data/statsItem.ts
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 
 export interface StatsItem {
+    id: string,
     icon: ReactElement,
     title: string,
     description: string
