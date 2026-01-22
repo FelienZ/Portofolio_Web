071935c269db9509aac5fe2126dc5e00d7687544
FelienZ <rajabfadh@gmail.com>
2026-01-22 22:18:55 +0700

    feat(filter): implement contribution filtering in contribution-graph

--- components/app-github.tsx
+++ components/app-github.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import {
   Activity,
   ContributionGraph,
@@ -20,70 +20,100 @@ import { useState } from "react";
 import { Spinner } from "./ui/spinner";
 import { Button } from "./ui/button";
 import { Badge } from "./ui/badge";
+import { Github, Link } from "lucide-react";
+import handleNavigateExternal from "@/utils/navigateExternal";
 
 const Githubcontributions = () => {
-const [isLoading, setIsLoading] = useState<boolean>(false)
-const contribution = GithubData({setIsLoading})
-/* function filterCurrentYear(data: Activity[]): Activity[] {
-  const thisYear = new Date().getFullYear().toString(); // mis. '2025'
-  return data.filter(item => item.date.startsWith(thisYear));
-} */
-  return (
-  isLoading ? (
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const contribution = GithubData({ setIsLoading });
+  function filterCurrentYear(data: Activity[]): Activity[] {
+    const lastYear = (new Date().getFullYear() - 1).toString();
+    const thisYear = new Date().getFullYear().toString();
+    return data.filter(
+      (item) =>
+        item.date.startsWith(lastYear) || item.date.startsWith(thisYear),
+    );
+  }
+  return isLoading ? (
     <Button variant="outline" disabled className="w-fit place-self-center">
-        <Spinner className="text-green-500"/>
-        Please wait
+      <Spinner className="text-green-500" />
+      Please wait
     </Button>
   ) : (
     <TooltipProvider>
-    <ContributionGraph data={/* filterCurrentYear */(contribution)} /* blockMargin={6} blockSize={16} fontSize={14} */>
-      <ContributionGraphCalendar className="py-3 [&::-webkit-scrollbar]:h-1.5
+      <ContributionGraph
+        data={filterCurrentYear(contribution)}
+        blockMargin={4}
+        blockSize={16}
+        fontSize={14}
+      >
+        <ContributionGraphCalendar
+          className="py-3 [&::-webkit-scrollbar]:h-1.5
       [&::-webkit-scrollbar-track]:rounded-full
       [&::-webkit-scrollbar-track]:bg-gray-100
       [&::-webkit-scrollbar-thumb]:rounded-full
       [&::-webkit-scrollbar-thumb]:bg-gray-300
       dark:[&::-webkit-scrollbar-track]:bg-neutral-700
-      dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-        {({ activity, dayIndex, weekIndex }) => (
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <g>
-                <ContributionGraphBlock
-                  activity={activity}
-                  className={cn(
-                    'data-[level="0"]:fill-[#ebedf0] dark:data-[level="0"]:fill-[#161b22]',
-                    'data-[level="1"]:fill-[#9be9a8] dark:data-[level="1"]:fill-[#0e4429]',
-                    'data-[level="2"]:fill-[#40c463] dark:data-[level="2"]:fill-[#006d32]',
-                    'data-[level="3"]:fill-[#30a14e] dark:data-[level="3"]:fill-[#26a641]',
-                    'data-[level="4"]:fill-[#216e39] dark:data-[level="4"]:fill-[#39d353]'
-                )}
-                  dayIndex={dayIndex}
-                  weekIndex={weekIndex}
-                />
-              </g>
-            </TooltipTrigger>
-            <TooltipContent>
-              <p className="font-semibold">{activity.date}</p>
-              <p>{activity.count} contributions</p>
-            </TooltipContent>
-          </Tooltip>
-        )}
-      </ContributionGraphCalendar>
-      <ContributionGraphFooter>
-         <ContributionGraphTotalCount>
-        {({ totalCount, year }) => (
-          <div className="flex items-center justify-end w-full gap-2">
-            <span className="text-muted-foreground text-sm">Year {year}:</span>
-            <Badge variant="secondary">
-              {totalCount.toLocaleString()} contributions
-            </Badge>
-          </div>
-        )}
-      </ContributionGraphTotalCount>
-      </ContributionGraphFooter>
-    </ContributionGraph>
-  </TooltipProvider>)
-  )
+      dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
+        >
+          {({ activity, dayIndex, weekIndex }) => (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <g>
+                  <ContributionGraphBlock
+                    activity={activity}
+                    className={cn(
+                      'data-[level="0"]:fill-[#ebedf0] dark:data-[level="0"]:fill-[#161b22]',
+                      'data-[level="1"]:fill-[#9be9a8] dark:data-[level="1"]:fill-[#0e4429]',
+                      'data-[level="2"]:fill-[#40c463] dark:data-[level="2"]:fill-[#006d32]',
+                      'data-[level="3"]:fill-[#30a14e] dark:data-[level="3"]:fill-[#26a641]',
+                      'data-[level="4"]:fill-[#216e39] dark:data-[level="4"]:fill-[#39d353]',
+                    )}
+                    dayIndex={dayIndex}
+                    weekIndex={weekIndex}
+                  />
+                </g>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="font-semibold">{activity.date}</p>
+                <p>{activity.count} contributions</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+        </ContributionGraphCalendar>
+        <ContributionGraphFooter>
+          <ContributionGraphTotalCount>
+            {({ totalCount, year }) => (
+              <div className="flex items-center justify-end w-full py-2 gap-2">
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      onClick={() =>
+                        handleNavigateExternal("https://github.com/FelienZ")
+                      }
+                      variant={"outline"}
+                      className="rounded-full size-6"
+                    >
+                      <Link className="p-0.5" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent className="flex items-center gap-2">
+                    <Github className="size-4" /> My Github
+                  </TooltipContent>
+                </Tooltip>
+                <span className="text-muted-foreground text-sm">
+                  Year {year - 1} - {year}:
+                </span>
+                <Badge variant="secondary">
+                  {totalCount.toLocaleString()} contributions
+                </Badge>
+              </div>
+            )}
+          </ContributionGraphTotalCount>
+        </ContributionGraphFooter>
+      </ContributionGraph>
+    </TooltipProvider>
+  );
 };
 
 export default Githubcontributions;
