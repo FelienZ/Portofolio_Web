66e7a3f8c73156c26bca9b325dafff896b279ca1
FelienZ <rajabfadh@gmail.com>
2026-01-23 08:40:34 +0700

    fix(hooks): implement use client for useWidth hooks & window checker before update width

--- hooks/useWidth.ts
+++ hooks/useWidth.ts
@@ -1,11 +1,15 @@
+"use client"
 import { useEffect, useState } from "react";
 
 export default function useWidth(){
     const [width, setWidth] = useState(0)
     useEffect(()=> {
         function changeWidth(){
+        if(typeof window !== undefined){
             setWidth(window.innerWidth)
         }
+        }
+        changeWidth()
         window.addEventListener("resize", changeWidth)
         return(()=>window.removeEventListener("resize", changeWidth))
     }, [])
